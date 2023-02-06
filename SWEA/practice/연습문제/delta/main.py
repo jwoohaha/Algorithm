229a06d78aecd6fa7c2cbe0d9c93ad3f7ccdc774
jwoohaha <jwoohaha@gmail.com>
2023-02-06 17:41:20 +0900

    update swea dir manually

--- SWEA/practice/연습문제/delta/main.py
+++ SWEA/practice/연습문제/delta/main.py
@@ -8,7 +8,7 @@ for tc in range(1, T+1):
     N = int(input())
     matrix = [list(map(int, input().split())) for _ in range(N)]
     sums = 0
-    for i in range(N-1):
+    for i in range(N):
         for j in range(N-1):
             sums += abs(matrix[i][j] - matrix[i][j+1])  # 열 간 차이
             sums += abs(matrix[j][i] - matrix[j+1][i])  # 행 간 차이
--- SWEA/practice/연습문제/subset/main.py
+++ SWEA/practice/연습문제/subset/main.py
@@ -7,6 +7,7 @@ T = int(input())
 for tc in range(1, T+1):
     nums = list(map(int, input().split()))
     result = 0
+    # 부분집합 구하기 (비트연산 이용)
     for i in range(1, 1<<10):  # 0인 경우는 제외해야 하므로 1에서 시작
         subset = []
         for j in range(10):
