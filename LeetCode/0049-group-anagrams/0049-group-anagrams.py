cc8d57debbbda9719f95cbcf80e443632afe3762
wooha <jwoohaha@gmail.com>
2023-02-12 21:09:55 +0900

    delete leetcode dir

--- LeetCode/0049-group-anagrams/0049-group-anagrams.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = {}
-
-        for word in strs:
-            key = ''.join(sorted(word))
-            anagrams.setdefault(key, [])
-            anagrams[key].append(word)
-        return(list(anagrams.values()))
