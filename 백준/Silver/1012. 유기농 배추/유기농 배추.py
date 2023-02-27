68e780655ab547b978bd2990f21732db3d128312
jwoohaha <jwoohaha@gmail.com>
2023-02-27 16:51:17 +0900

    [Silver II] Title: 유기농 배추, Time: 312 ms, Memory: 31556 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,37 +1,19 @@
+import sys
+sys.setrecursionlimit(10000)
 
-def count_worms(grid):
-    """
-    :type grid: List[List[str]]
-    :rtype: int
-    """
-    r = len(grid)  # row 수
-    c = len(grid[0])  # col 수
-    visited = set()  # 방문한 지점 저장
-    stk = []
-    cnt = 0  # 섬 갯수 카운트
-    # grid를 탐색하며 확인
-    for i in range(r):
-        for j in range(c):
-            # 시작점 찾기
-            if (i, j) not in visited and grid[i][j] == 1:
-                row, col = i, j
-                visited.add((i, j))
-                cnt += 1
-                # bfs로 탐색
-                while True:
-                    # 주변 노드 탐색
-                    for di, dj in [[-1, 0], [0, 1], [1, 0], [0, -1]]:  # 상 우 하 좌
-                        ni = row + di
-                        nj = col + dj
-                        if 0 <= ni < r and 0 <= nj < c:
-                            # 방문하지 않고 땅이면 스택에 추가, 방문 표시
-                            if (ni, nj) not in visited and grid[ni][nj] == 1:
-                                visited.add((ni, nj))
-                                stk.append((ni, nj))
-                    if not stk:
-                        break
-                    row, col = stk.pop()
-    return cnt
+
+def dfs(i, j):
+    # 배추가 아니면 종료
+    if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != 1:
+        return
+    grid[i][j] = 0
+    # 동서남북 탐색
+    dfs(i + 1, j)
+    dfs(i - 1, j)
+    dfs(i, j + 1)
+    dfs(i, j - 1)
+
+
 
 T = int(input())
 for tc in range(1, T+1):
@@ -42,4 +24,12 @@ for tc in range(1, T+1):
     for _ in range(K):
         m, n = map(int, input().split())
         grid[n][m] = 1
-    print(count_worms(grid))
+
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                dfs(i, j)
+                # 모든 배추 탐색 후 카운트 1 증가
+                count += 1
+    print(count)
