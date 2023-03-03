cfbf1487d45c2d31594c39262337e3c5fddf8791
jwoohaha <jwoohaha@gmail.com>
2023-03-03 15:16:30 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 696 ms, Memory: 65172 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,30 @@
+import sys
+sys.setrecursionlimit(5000)
+input = sys.stdin.readline
+
+# main
+N, M = map(int, input().split())  # 정점의 개수, 간선의 개수
+graph = [[] for i in range(N+1)]
+visited = [False] * (N+1)
+# 그래프 생성
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+
+def dfs(i):
+    visited[i] = True
+    for w in graph[i]:
+        if not visited[w]:
+            dfs(w)
+    return
+
+# 아직 방문하지 못한 점을 만나면 분리된 component -> cnt + 1
+component_cnt = 0
+for i in range(1, N+1):
+    if not visited[i]:
+        component_cnt += 1
+        dfs(i)
+
+print(component_cnt)
